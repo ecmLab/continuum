dbcaaf9d8d1237aaf15fe7d9a302c4d2f24d08dd
HowardTu <howard@HowardTus-MacBook-Pro.local>
2023-05-28 16:25:20 -0400

    052823-paper_yaoyan

--- deecm/paper_yaoyan_agPercolation/pack/myjob.sh
+++ deecm/paper_yaoyan_agPercolation/pack/myjob.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
-# Job name:
-#SBATCH --job-name=NMC_LPS
-#
-# Partition:
+#SBATCH --job-name=Ag_aC
+#SBATCH --account=membrane
 #SBATCH --partition=tier3
 #
 # Processors:
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=20
-#SBATCH --cpus-per-task=1
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=4
 #
 # Wall clock limit:
-#SBATCH --time=00:30:00
+#SBATCH --time=02:00:00
 #SBATCH --mem=10g
 
-
 ## Commands to run:
-
+spack load lammps@20220107
 srun lmp -log none -in mr.in
