578981145f196f0dacf6ad4ae22f34f0f39c15b1
ecmLab <howietu@berkeley.edu>
2023-06-30 11:55:12 -0400

    063023-paper_yaoyan

--- deecm/paper_yaoyan_agPercolation/0_model/lognorm/mdl_sub.sh
+++ deecm/paper_yaoyan_agPercolation/0_model/lognorm/mdl_sub.sh
@@ -11,8 +11,8 @@
 #SBATCH --cpus-per-task=1
 #
 # Wall clock limit:
-#SBATCH --time=10:00:00
-#SBATCH --mem=6g
+#SBATCH --time=24:00:00
+#SBATCH --mem=60g
 
 ## Commands to run:
 srun matlab -nodisplay -nosplash < mdl_run.m
