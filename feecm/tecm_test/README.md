d954554b4f1df6d7aae38532fcfc1ce5db978002
ecmLab <howietu@berkeley.edu>
2025-09-01 14:28:33 -0400

    090125_claudeCreateTECM

---
+++ feecm/tecm_test/README.md
@@ -0,0 +1,6 @@
+tecm_test
+=====
+
+Fork "tecm_test" to create a new MOOSE-based application.
+
+For more information see: [https://mooseframework.org/getting_started/new_users.html#create-an-app](https://mooseframework.org/getting_started/new_users.html#create-an-app)
---
+++ feecm/tecm_test/src/kernels/RankOneDivergence.C
@@ -0,0 +1,26 @@
+#include "RankOneDivergence.h"
+
+registerMooseObject("tecm_testApp", RankOneDivergence);
+
+InputParameters
+RankOneDivergence::validParams()
+{
+  InputParameters params = ADKernel::validParams();
+  params.addClassDescription("This class implements the weak form for the divergence of a vector.");
+  params.addRequiredParam<MaterialPropertyName>("vector", "The vector");
+  params.addParam<Real>("factor", 1, "The multiplication factor");
+  return params;
+}
+
+RankOneDivergence::RankOneDivergence(const InputParameters & params)
+  : ADKernel(params),
+    _vector(getADMaterialProperty<RealVectorValue>("vector")),
+    _factor(getParam<Real>("factor"))
+{
+}
+
+ADReal
+RankOneDivergence::computeQpResidual()
+{
+  return -_factor * _vector[_qp] * _grad_test[_i][_qp];
+}
