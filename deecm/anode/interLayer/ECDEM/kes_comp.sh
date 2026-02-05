d574bdf2892baf2d0325f792356472d5d0d07799
Joseph Vazquez <jmv8431@rit.edu>
2026-02-05 16:43:56 -0500

    Update simulation input files and job script for improved accuracy and performance

---
+++ deecm/anode/interLayer/ECDEM/kes_comp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=COM_Ag_M1C5_vr15_fp250_sp14
+#SBATCH --partition=RM-shared
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=64
+#SBATCH --time=04:00:00
+#SBATCH --account=mat250014p
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=vazquezm
+
+# Load required modules LIGGGHTS
+module load gcc/10.2.0 openmpi/5.0.3-gcc13.2.1 cuda/11.7.1
+
+# The simulations will run in the order listed below.
+mpiexec -np $SLURM_NTASKS /ocean/projects/mat250014p/shared/software/LIGGGHTS-PUBLIC/src/lmp_auto -in pck.in
